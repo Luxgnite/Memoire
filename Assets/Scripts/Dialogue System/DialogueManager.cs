6b35031a51a687b2adc4919e3bf990efd00b7d8d
Ewan Dubois <ewan.dubois@live.fr>
2021-07-13 15:07:03 +0200

    Import Dialogue System + Word Click Detection

---
+++ Assets/Scripts/Dialogue System/DialogueManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager _instance = null;
+
+    public Canvas messageCanvas;
+    public Message dialogueActif;
+    public Message messagePrefab;
+    [Header("Vitesse de l'effet machine à écrire. Caractère par secondes")]
+    [Range(1, 200)]
+    public int typeSpeed;
+
+    private GameObject pnjActuel;
+    private Queue<DialoguePhrase> phrases;
+ 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Singleton Pattern
+        if (_instance == null)
+            _instance = this;
+        else if (_instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        phrases = new Queue<DialoguePhrase>();
+    }
+
+    public void ChargerDialogue(Dialogue dialogue, GameObject pnj)
+    {
+        phrases.Clear();
+        GameManager._instance.dialogue = true;
+
+        pnjActuel = pnj;
+        foreach(DialoguePhrase phrase in dialogue.phrases)
+        {
+            phrases.Enqueue(phrase);
+        }
+
+        AfficherProchainePhrase();
+    }
+
+    public void AfficherProchainePhrase()
+    {
+        if(phrases.Count != 0)
+        {
+            DetruirePhrasePrecedente();
+
+            DialoguePhrase phrase = phrases.Dequeue();
+
+            dialogueActif = Instantiate(messagePrefab, new Vector3(0, 0, -1000), Quaternion.identity);
+            dialogueActif.gameObject.transform.SetParent(messageCanvas.transform, false);
+
+            dialogueActif.displayText = phrase.texte;
+            
+            if(phrase.emetteurValue == DialoguePhrase.Emetteur.JOUEUR)
+                dialogueActif.target = GameManager._instance.player;
+            else if (phrase.emetteurValue == DialoguePhrase.Emetteur.PNJ)
+                dialogueActif.target = pnjActuel;
+            dialogueActif.timeToDie = 0f;
+        }
+        else
+        {
+            GameManager._instance.dialogue = false;
+        }
+    }
+
+    //A changer comme système
+    public void DetruirePhrasePrecedente()
+    {
+        if (dialogueActif != null)
+            Destroy(dialogueActif.gameObject);
+    }
+}
---
+++ Assets/Scripts/Dialogue System/Interactible.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public string texteAide;
+    public bool fenetre = false;
+
+    private Message instanceTexteAide;
+    private void OnMouseDown()
+    {
+        if(instanceTexteAide != null)
+            DestroyImmediate(instanceTexteAide.gameObject);
+
+        if (fenetre && !GameManager._instance.dialogue)
+        {
+            GameManager._instance.ChangeScene("Contemplation");
+        }
+        else
+        {
+            DialogueManager._instance.ChargerDialogue(dialogue, this.gameObject);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if(DialogueManager._instance.dialogueActif == null)
+        {
+            Debug.Log("On enter");
+            instanceTexteAide = Instantiate(DialogueManager._instance.messagePrefab, new Vector3(0, 0, -1000), Quaternion.identity);
+            instanceTexteAide.gameObject.transform.SetParent(DialogueManager._instance.messageCanvas.transform, false);
+
+            instanceTexteAide.typeWrite = false;
+            instanceTexteAide.displayText = "<i>" + texteAide + "</i>";
+
+            if (fenetre)
+                instanceTexteAide.target = GameManager._instance.player;
+            else
+                instanceTexteAide.target = this.gameObject;
+            instanceTexteAide.timeToDie = 0f;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if(instanceTexteAide != null)
+        {
+            instanceTexteAide.timeToDie = 1f;
+            instanceTexteAide.SetAutoDestruction();
+        }
+    }
+}
